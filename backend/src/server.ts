4601f0891c7b1da7e188b817d3d002a88908b3d9
Marwane Assou <marwane.assou@gmail.com>
2024-12-07 17:57:17 +0100

    test execute stored procedure

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,6 +1,7 @@
 
 import express, {Express, Request, Response} from 'express';
 import { ConnectDatabase } from './dbConnection';
+import { QueryResult } from 'mysql2';
 
 const app: Express = express();
 const port = 3000;
@@ -13,7 +14,15 @@ app.listen(port, ()=> {
 console.log(`[Server]: I am running at https://localhost:${port} `);
 });
 
-const dt =ConnectDatabase.createConnection();
+const db =ConnectDatabase.createConnection();
+db.query(`CALL showUsers(?)`,[1], (err, results : any) => {
+    if (err) {
+        console.error('Error executing stored procedure:', err);
+    } else {
+        console.log(results[0][0]);
+    }
+});
+
 
 
 
