91773a0e2d709f00cb78a71849bef9639cc33714
yass3rbouh1a <yasser.ali.bouhia@gmail.com>
2025-03-06 09:40:21 +0000

    XD

---
+++ frontend/smart_house/src/App.jsx
@@ -0,0 +1,11 @@
+function App() {
+  return (
+    <div class="container mt-5">
+  <h1 class="text-center text-4xl font-bold">Hello, World!</h1>
+  <button class="btn btn-primary">Bootstrap Button</button>
+</div>
+
+  );
+}
+
+export default App;
